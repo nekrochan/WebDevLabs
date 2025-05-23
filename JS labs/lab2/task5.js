1ceb72e2ab7c98fdd8390f2b8444febe18bd4d62
nekrochan <rubus.saxatilis.99@gmail.com>
2025-05-23 23:54:19 +0300

    lab2 task5: added chek for 8-bit and binarity

--- JS labs/lab2/task5.js
+++ JS labs/lab2/task5.js
@@ -1,5 +1,15 @@
 function binaryToDecimal(binary){
     let binaryStr = String(binary)
+
+    //проверка на 8-битность
+    if (binaryStr.length!==8) return `${binary}: Not 8-bit. Length: ${binaryStr.length}`
+
+    //проверка на бинарность
+    for (let i = binaryStr.length-1; i >= 0; i--) {
+        if (String(binaryStr[i])!=="0" && String(binaryStr[i])!=="1") return `${binary}: Not binary. Contains:  ${binaryStr[i]}`
+    }
+
+    //перевод в десятичные
     let decimal = 0
     for (let i = binaryStr.length-1; i >= 0; i--) {
         let order = binaryStr.length - 1 - i
@@ -11,3 +21,5 @@ function binaryToDecimal(binary){
 
 console.log(binaryToDecimal('00001001'))
 console.log(binaryToDecimal('11110000'))
+console.log(binaryToDecimal('00001002'))
+console.log(binaryToDecimal('111'))
