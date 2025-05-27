fe5b9300052fb1b54251420a428963d01b8af2fd
nekrochan <rubus.saxatilis.99@gmail.com>
2025-05-27 19:11:53 +0300

    lab6 task7 upgrade

--- JS labs/lab6/task7/app.js
+++ JS labs/lab6/task7/app.js
@@ -1,45 +1,32 @@
 function lockedProfile() {
-
     let profiles = document.querySelectorAll('.profile')
 
     profiles.forEach(profile => {
-
         let hidden = profile.querySelector('div[id$="HiddenFields"]')
         let button = profile.querySelector('button')
         let radios = profile.querySelectorAll('input[type="radio"]')
 
-        radios.forEach(radio => { radio.addEventListener('change', changeRadioCondition) })
+        radios.forEach(radio => radio.addEventListener('change', changeRadioCondition))
         button.addEventListener('click', changeButtonCondition)
 
-        // вспомогательные функции:
+        changeRadioCondition()
 
         function changeRadioCondition() {
-
-            let isLocked = profile.querySelector('input[value="lock"]').checked
-            if (isLocked) {
-                hidden.style.display = 'none'
-                button.textContent = 'Show more'
-            }
-
+            let isLocked = profile.querySelector('input[value="lock"]').checked;
+            button.disabled = isLocked
         }
 
         function changeButtonCondition() {
-
             let isLocked = profile.querySelector('input[value="lock"]').checked
-            if (!isLocked) { // сворачивать/разворачивать можно только незаблокированных
-                // без проверки '' открывается только при втором нажатии на кнопку
-                if ((hidden.style.display == 'none') || (hidden.style.display == '')) {
-                    // кнопка разворачивает содержимое
+            if (!isLocked) {
+                if (hidden.style.display === 'none' || hidden.style.display === '') {
                     hidden.style.display = 'block'
-                    button.textContent = 'Hide'
+                    button.textContent = 'Hide it'
                 } else {
-                    // кнопка сворачивает содержимое
                     hidden.style.display = 'none'
                     button.textContent = 'Show more'
                 }
             }
-
         }
-
     })
 }
